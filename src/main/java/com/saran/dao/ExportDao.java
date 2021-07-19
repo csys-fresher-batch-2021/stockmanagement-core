85df6023e559f764fead433d4a2f6ad051135f08
saran <rs.saransaro@gmail.com>
2021-07-19 09:49:59 +0530

    ADDExport feature

--- src/main/java/com/saran/dao/ExportDao.java
+++ src/main/java/com/saran/dao/ExportDao.java
@@ -2,9 +2,8 @@ package com.saran.dao;
 
 import com.saran.model.Export;
 
-	public interface ExportDao {
+public interface ExportDao {
 
-		boolean save(Export export) throws Exception;
-		
+	boolean save(Export export) throws Exception;
+	
 }
-
