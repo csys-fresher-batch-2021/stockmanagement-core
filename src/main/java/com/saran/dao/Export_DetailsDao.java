eafeed18cee097ca26e7b157a916817dbd39864d
saran <rs.saransaro@gmail.com>
2021-07-19 12:53:00 +0530

    Name Convention bug fixed

---
+++ src/main/java/com/saran/dao/Export_DetailsDao.java
@@ -0,0 +1,9 @@
+package com.saran.dao;
+
+import com.saran.model.Export_Details;
+
+public interface Export_DetailsDao {
+
+	boolean save(Export_Details export_Details) throws Exception;
+	
+}
---
+++ src/test/java/com/saran/model/Export_DetailsTest.java
@@ -0,0 +1,18 @@
+package com.saran.model;
+import java.time.LocalDate;
+public class Export_DetailsTest {
+
+	public static void main(String[] args) {
+		LocalDate Export_date1 = LocalDate.parse("2021-06-12");
+		Export_Details Export1= new Export_Details (12346, 1267, Export_date1, "Pondicherry", 34890);
+		System.out.println(Export1.toString());
+		
+		LocalDate Export_date2 = LocalDate.parse("2021-06-07");
+		Export_Details Export2= new Export_Details (12347, 3645, Export_date2, "Chennai", 15870);
+		System.out.println(Export2.toString());
+		
+		LocalDate Export_date3 = LocalDate.parse("2021-06-09");
+		Export_Details Export3= new Export_Details (12348, 3648 , Export_date3 , "Chennai", 15090);
+		System.out.println(Export3.toString());
+	}
+}
